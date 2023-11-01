15ae5e1c17ca27e5604c99003a0deca614a2ab19
Dhanush MC <dhanushmc93@gmail.com>
2023-11-02 00:37:38 +0530

    c

--- build/utils/jwt.js
+++ build/utils/jwt.js
@@ -25,11 +25,6 @@ const sendToken = (user, statusCode, res) => {
     const refreshToken = user.SignRefreshToken();
     // upload session to redis
     redis_1.redis.set(user._id, JSON.stringify(user));
-    // parse environment variables to integrates with fallback values
-    // only set secure to true in production
-    // if (process.env.NODE_ENV === "production") {
-    //   accessTokenOptions.secure = true;
-    // }
     res.cookie("access_token", accessToken, exports.accessTokenOptions);
     res.cookie("refresh_token", refreshToken, exports.refreshTokenOptions);
     res.status(statusCode).json({
