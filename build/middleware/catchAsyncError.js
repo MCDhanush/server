6be8c602fe66d575daadcbfbef9a1570dc066a5e
Ayush Srivastav <ayush@downtask.com>
2023-11-01 21:04:41 +0530

    removed build folder and fixed the build command

--- build/middleware/catchAsyncError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CatchAsyncError = void 0;
-// catch async error
-const CatchAsyncError = (thefunciton) => (req, res, next) => {
-    Promise.resolve(thefunciton(req, res, next)).catch(next);
-};
-exports.CatchAsyncError = CatchAsyncError;
